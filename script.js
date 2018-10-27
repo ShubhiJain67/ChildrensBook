995adaa766dbc5226f722c8e72ad5d3caffacac0
Shubhi Jain <jainshubhi67@gmail.com>
2018-10-28 04:50:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+function next1(){
+    location.href="pages/page1.html"
+}
+
+function next2(){
+    location.href="pages/page2.html"
+}
+
+function next3(){
+    location.href="pages/page3.html"
+}
+
+function next4(){
+    location.href="pages/page4.html"
+}
+
+function next5(){
+    location.href="pages/page5.html"
+}
+
+function next6(){
+    location.href="pages/page6.html"
+}
+
+function back1(){
+    location.href="index.html"
+}
+
+function back2(){
+    location.href="pages/page1.html"
+}
+
+function back3(){
+    location.href="pages/page2.html"
+}
+
+function back4(){
+    location.href="pages/page3.html"
+}
+
+function back5(){
+    location.href="pages/page4.html"
+}
+
+function back6(){
+    location.href="pages/page5.html"
+}
+
+function close(){
+    location.href="../index.html"
+}
